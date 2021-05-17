316e64a8ca932cdc0ae1c0630493c0dccf51f35c
kangseokgyu <ksg890528@gmail.com>
2021-05-17 16:17:41 +0900

    String isMacFormat 테스트 케이스 추가

--- test/mac_addr_test.cpp
+++ test/mac_addr_test.cpp
@@ -5,8 +5,10 @@
 #include "mac_addr.h"
 
 TEST(MacAddr, MacAddr) {
-  ASSERT_EQ(MacAddr(0), MacAddr(std::vector<uint8_t>(6, 0)));
-  ASSERT_EQ(MacAddr(281474976710655), MacAddr(std::vector<uint8_t>(6, 255)));
+  ASSERT_EQ(MacAddr(0),
+            MacAddr(std::vector<uint8_t>(6, 0))); // 00:00:00:00:00:00
+  ASSERT_EQ(MacAddr(281474976710655),
+            MacAddr(std::vector<uint8_t>(6, 255))); // ff:ff:ff:ff:ff:ff
 }
 
 TEST(MacAddr, isMacFormatString) {
@@ -21,4 +23,6 @@ TEST(MacAddr, isMacFormatString) {
   ASSERT_FALSE(MacAddr::isMacFormat(std::string("000000000000")));
   ASSERT_FALSE(MacAddr::isMacFormat(std::string("ffffffffffff")));
   ASSERT_FALSE(MacAddr::isMacFormat(std::string("ff=ff=ff=ff=ff=ff")));
+  ASSERT_FALSE(MacAddr::isMacFormat(std::string("tt-tt-tt-tt-tt-tt")));
+  ASSERT_FALSE(MacAddr::isMacFormat(std::string("aa-aa-aa-aa-aa-tt")));
 }
